b93796868ebea8c8871f742506f836f0529f0d93
emil_ <emil.kushner.stud@mail.ru>
2023-09-27 19:41:20 +0700

    Вроде готовый проект

--- app/src/main/java/com/example/lab2withoutgit/MainActivity.kt
+++ app/src/main/java/com/example/lab2withoutgit/MainActivity.kt
@@ -11,16 +11,25 @@ class MainActivity : ComponentActivity() {
         //объявление экземпляров
         val button_solve = findViewById<Button>(R.id.solution)
         val input_condition = findViewById<EditText>(R.id.input_text)
+        val output_result = findViewById<EditText>(R.id.result)
+        val output_term = findViewById<EditText>(R.id.lastTerm)
+        val output_itterations = findViewById<EditText>(R.id.Itterations)
         button_solve.setOnClickListener {
             val condition = input_condition.text.toString().toDouble()
             var result: Double = 0.0
             var n: Int = 1
             var factorial: Int = 1
+            //цикл прибавления
             do {
                 result += 1 / factorial
                 n += 2
                 factorial *= n * (n - 1)
             }while(condition < 1/factorial)
+            factorial /= (n * (n - 1))
+            //вывод результата
+            output_result.setText(result.toString())
+            output_term.setText((1/factorial).toString())
+            output_itterations.setText((n/2).toString())
         }
     }
 }
