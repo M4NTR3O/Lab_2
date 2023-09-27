5d6da6abe6bf773944153bbe40ec2cec2537231d
emil_ <emil.kushner.stud@mail.ru>
2023-09-27 19:50:43 +0700

    Готовый проект

--- app/src/main/java/com/example/lab2withoutgit/MainActivity.kt
+++ app/src/main/java/com/example/lab2withoutgit/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.ComponentActivity
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         //объявление экземпляров
         val button_solve = findViewById<Button>(R.id.solution)
         val input_condition = findViewById<EditText>(R.id.input_text)
