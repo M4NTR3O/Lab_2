271260c3e2ac588180655de85e70577509438691
emil_ <emil.kushner.stud@mail.ru>
2023-09-27 19:14:54 +0700

    Готовый дизайн, объявленные экземпляры кнопки и входного текста, а также создано событие для кнопки

--- app/src/main/java/com/example/lab2withoutgit/MainActivity.kt
+++ app/src/main/java/com/example/lab2withoutgit/MainActivity.kt
@@ -1,46 +1,18 @@
 package com.example.lab2withoutgit
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.lab2withoutgit.ui.theme.Lab2withoutGitTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Lab2withoutGitTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        //объявление экземпляров
+        val button_solve = findViewById<Button>(R.id.solution)
+        val input_condition = findViewById<EditText>(R.id.input_text)
+        button_solve.setOnClickListener {
+
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab2withoutGitTheme {
-        Greeting("Android")
     }
 }
